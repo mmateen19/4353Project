cfc8dd887538bcb779fc43c003ebe7afa3cfcf2e
MessiMatt54321 <matthewjo.philip@gmail.com>
2022-02-17 22:21:55 -0600

    small css changes to login/register

--- gasprice-app/src/components/Register.js
+++ gasprice-app/src/components/Register.js
@@ -2,7 +2,11 @@ import React, { useState } from "react";
 import "./Register.css";
 
 const Register = () => {
-  return <div>Test</div>;
+  return (
+    <div className="display-container">
+      <div>Test</div>
+    </div>
+  );
 };
 
 export default Register;
