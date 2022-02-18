6e0223fc2c091d44e3486966228faa4ad47471cb
MessiMatt54321 <matthewjo.philip@gmail.com>
2022-02-18 15:38:20 -0600

    profileformcss

--- gasprice-app/src/components/Profile.js
+++ gasprice-app/src/components/Profile.js
@@ -2,13 +2,16 @@ import React from "react";
 import "./Profile.css";
 import CustNav from "./CustNav";
 import Form from "./Form";
+import "./Form.css";
 
 const Profile = () => (
   <div className="Profile">
     <div className="navbar">
       <CustNav />
     </div>
-    <Form />
+    <div className="Form">
+      <Form />
+    </div>
   </div>
 );
 export default Profile;
