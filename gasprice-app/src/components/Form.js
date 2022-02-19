8e0fe3d62a962682bb5ab8eceea2691fef11bcfd
MessiMatt54321 <matthewjo.philip@gmail.com>
2022-02-19 12:37:05 -0600

    form style inprog

--- gasprice-app/src/components/Form.js
+++ gasprice-app/src/components/Form.js
@@ -1,104 +1,95 @@
 import React from "react";
 import "./Form.css";
 
-
-
-
 const Form = ({
-    firstName, 
-    lastName,
-    addy1,
-    addy2,
-    city,
-    state,
-    zipCode,
-    companyName,
-    firstNameInput, lastNameInput, 
-    addy1Input, addy2Input,
-    cityInput, stateInput, zipCodeInput,
-    companyNameInput,
-    setFirstName, setLastName,
-    setAddy1, setAddy2, 
-    setCity, setState,setZipCode,
-    setCompanyName,
-    setGoToProfile,
-    setErrorMessages,
-  
-  }) => {
-        const errors={
-         f_name: "required",
-         l_name: "required",
-         address1: "required",
-         city_: "required",
-         state_: "required",
-         zip_: "required",
+  firstName,
+  lastName,
+  addy1,
+  addy2,
+  city,
+  state,
+  zipCode,
+  companyName,
+  firstNameInput,
+  lastNameInput,
+  addy1Input,
+  addy2Input,
+  cityInput,
+  stateInput,
+  zipCodeInput,
+  companyNameInput,
+  setFirstName,
+  setLastName,
+  setAddy1,
+  setAddy2,
+  setCity,
+  setState,
+  setZipCode,
+  setCompanyName,
+  setGoToProfile,
+  setErrorMessages,
+}) => {
+  const errors = {
+    f_name: "required",
+    l_name: "required",
+    address1: "required",
+    city_: "required",
+    state_: "required",
+    zip_: "required",
   };
-  
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (firstNameInput=""){
-      setErrorMessages({name: "f_name", message:errors.f_name});
-    }else if (lastNameInput=""){
-      setErrorMessages({name: "l_name", message:errors.l_name});
-    } else if (addy1Input=""){
-      setErrorMessages({name: "address1", message:errors.address1});
-    }else if (cityInput=""){
-      setErrorMessages({name: "city_", message:errors.city_});
-    }else if (stateInput=""){
-      setErrorMessages({name: "state_", message:errors.state_});
-    }else if (zipCodeInput=""){
-      setErrorMessages({name: "zip_", message:errors.zip_});
+    if ((firstNameInput = "")) {
+      setErrorMessages({ name: "f_name", message: errors.f_name });
+    } else if ((lastNameInput = "")) {
+      setErrorMessages({ name: "l_name", message: errors.l_name });
+    } else if ((addy1Input = "")) {
+      setErrorMessages({ name: "address1", message: errors.address1 });
+    } else if ((cityInput = "")) {
+      setErrorMessages({ name: "city_", message: errors.city_ });
+    } else if ((stateInput = "")) {
+      setErrorMessages({ name: "state_", message: errors.state_ });
+    } else if ((zipCodeInput = "")) {
+      setErrorMessages({ name: "zip_", message: errors.zip_ });
+    } else {
+      //gonna navigate to home
     }
-
   };
 
-return(
-  <div className="display-container">
-    <h1>Form</h1>
+  return (
+    <div className="display-container">
+      <h1>Account Information</h1>
+      <div className="form-sections">
+        <section className="input-sections">
+          <label for="FullName">Full Name: </label>
+          <input name="FullName" type="text"></input>
 
-    <section className="Input-Sections">
-      <label for="FirstName">First Name: </label>
-      <input name="FirstName" type="text" className="Left-Box"></input>
+          <label for="CompanyName">Company Name: </label>
+          <input name="CompanyName" type="text"></input>
+        </section>
+        <section className="input-sections">
+          <label for="Addy1">Address 1: </label>
+          <input name="Addy1" type="text"></input>
 
-      <label for="LastName">Last Name: </label>
-      <input name="LastName" type="text"></input>
-    </section>
+          <label for="Addy2">Address 2: </label>
+          <input name="Addy2" type="text"></input>
+        </section>
+        <section className="input-sections">
+          <label for="City">City: </label>
+          <input name="City" type="text"></input>
 
-    <section className="Input-Sections">
-      <label for="Addy1">Address 1: </label>
-      <input name="Addy1" type="text" className="Left-Box"></input>
+          <label for="Zip">Zip Code: </label>
+          <input name="Zip" type="text"></input>
 
-      <label for="Addy2">Address 2: </label>
-      <input name="Addy2" type="text"></input>
-    </section>
-
-    <section className="Input-Sections">
-      <label for="City">City: </label>
-      <input name="City" type="text" className="small-input"></input>
-
-      <label for="State">State: </label>
-      <select name="State" className="small-input"></select>
-
-      <label for="Zip">Zip Code: </label>
-      <input name="Zip" type="text" className="small-input"></input>
-
-     
-    </section>
-
-    <section className="Input-Sections">
-      <label for="CompanyName">Comapny Name: </label>
-      <input name="CompanyName" type="text" className="small-input"></input>
-
-    </section>
-
-    <button>onClick{handleSubmit}</button>
-      <input type="submit"/>
-
-
-
-  </div>
-);
-
+          <label for="State">State: </label>
+          <select name="State"></select>
+        </section>
+      </div>
+      <button></button>
+      <input type="submit" onSubmit={handleSubmit} />
+    </div>
+  );
 };
 
 export default Form;
