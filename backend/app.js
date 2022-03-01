af196f6e94ff8996de6b54ad287f43e4d1a8a249
MessiMatt54321 <matthewjo.philip@gmail.com>
2022-03-01 16:05:54 -0600

    testing push

--- backend/app.js
+++ backend/app.js
@@ -4,4 +4,6 @@ const port = 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(port, () => console.log("Example app listening on port ${port}!"));
+app.listen(process.env.PORT || "5000", () => {
+  console.log(`Server is running on port: ${process.env.PORT || "5000"}`);
+});
