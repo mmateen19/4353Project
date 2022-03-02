d467e0cdc154777d3e082cd0a05324d093b45f0c
MessiMatt54321 <matthewjo.philip@gmail.com>
2022-03-01 21:19:31 -0600

    Revert "testing push"
    This reverts commit af196f6e94ff8996de6b54ad287f43e4d1a8a249.
    
    revert to old main

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,4 @@ const port = 5000;
 
 app.get("/", (req, res) => res.send("Hello World!"));
 
-app.listen(process.env.PORT || "5000", () => {
-  console.log(`Server is running on port: ${process.env.PORT || "5000"}`);
-});
+app.listen(port, () => console.log("Example app listening on port ${port}!"));
