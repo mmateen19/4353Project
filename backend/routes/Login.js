62a756dca0ed8cae6361f65a55cd8eb710e92da2
aoquresh <aoquresh@gmail.com>
2022-04-03 18:53:58 -0500

    DB code clean up

--- backend/routes/Login.js
+++ backend/routes/Login.js
@@ -35,27 +35,6 @@ const registerUser = (req, res, next) =>
     })
     
 
-
-
-    // let data = {
-    //     username: req.body.username,
-    //     password: req.body.password,
-    //     history: [], //an array of json objects
-    //     fullName: "",
-    //     company: "",
-    //     address1: "",
-    //     address2: "",
-    //     city: "",
-    //     zipcode: "",
-    //     state: "",
-    //     id: database.length + 1,
-    //     token: "",
-    //   };
-    
-    //   database.push(data)
-    //   //console.log(database)
-    //   res.json(database);
-
 }
 
 
@@ -97,37 +76,6 @@ const logUserIn = (req, res, nex) =>
 
 
     })
-
-
-
-//     console.log(req.body);
-  // const userData = database.find(
-  //   (user) => user.username === req.body.username
-  // );
-  // let pass;
-  // if (userData) {
-  //   // console.log(userData)
-  //   if (userData.password === req.body.password){
-  //     req.session.user = userData
-  //     //console.log(req.session.user)
-  //     pass = userData.password;
-
-  //     //Auth
-  //     const id = userData.id
-      
-  //     const token = jwt.sign({id}, process.env.ACCESS_TOKEN_SECRET);
-
-  //     userData.token = token; 
-
-  //     res.json({auth: true, token: token, userData: userData})
-  //   }
-  //   else {
-  //     res.json({auth: false, message: "Not Found Pass!"})
-  //   }
-  // } else {
-  //   res.json({auth: false, message: "Not Found User!"})
-  // }
-
 }
 
 
