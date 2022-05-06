b79a7bbeedb5326b4c129d34d42f38f131f79d5d
Maryum Mateen <maryummateen19@gmail.com>
2022-05-06 15:22:50 -0500

    updating tests

--- backend/routes/FuelQuote.js
+++ backend/routes/FuelQuote.js
@@ -2,48 +2,6 @@ const { check, validationResult } = require("express-validator");
 const database = require("../database/database.js");
 const client = require("../database/database");
 
-// validation method
-
-// const validate = (method) => {
-//   switch (method) {
-//     case "getQuote": {
-//       return [
-//         check("userId").exists().not().isEmpty().withMessage("Bad Request"),
-
-//         check("location").exists().not().isEmpty().withMessage("Bad Request"),
-
-//         check("gallons")
-//           .exists()
-//           .not()
-//           .isEmpty()
-//           .isNumeric()
-//           .custom((val, { req }) => {
-//             return req.body.gallons >= 0;
-//           })
-//           .withMessage("Bad Request"),
-//       ];
-//     }
-
-//     case "saveQuote": {
-//       return [
-//         check("userId").exists().not().isEmpty().withMessage("Bad Request"),
-
-//         check("location").exists().not().isEmpty().withMessage("Bad Request"),
-
-//         check("gallons").exists().not().isEmpty().withMessage("Bad Request"),
-
-//         check("quote").exists().not().isEmpty().withMessage("Bad Request"),
-//       ];
-//     }
-
-//     case "getHistory": {
-//       return [
-//         check("userId").exists().not().isEmpty().withMessage("Bad Request"),
-//       ];
-//     }
-//   }
-// };
-
 const saveQuote = (req, res, next) => {
   //console.log(req.body);
   id = req.body.id;
@@ -64,7 +22,6 @@ const saveQuote = (req, res, next) => {
     }
   );
 };
-
 
 const getHistory = (req, res) => {
   id = req.body.id; 
@@ -87,35 +44,8 @@ const getHistory = (req, res) => {
   )
 };
 
-// const getQuote = async (req, res) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-
-//   if (!req.body.userId || !req.body.location || !req.body.gallons) {
-//     return res.sendStatus(400);
-//   }
-//   return res.status(200).json({ errors: errors.array() });
-// };
-
-// const getHistory = (req, res, next) => {
-//   const errors = validationResult(req);
-//   if (!errors.isEmpty()) {
-//     return res.status(400).json({ errors: errors.array() });
-//   }
-//   const userId = req.body.userId;
-//   if (!userId) {
-//     return res.sendStatus(400);
-//   } else {
-//     return res.status(200).json({ errors: errors.array() });
-//   }
-//};
 
 module.exports = {
-  //getQuote,
-  //getHistory,
-  //validate,
   saveQuote,
   getHistory,
 };
